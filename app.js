94f0a145a182033aa659181978e2ec07e761c4a7
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-02-11 16:44:53 -0800

    Create linked list factory
    Added a method to append new nodes to the list

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const nodeFactory = (value = null, nextNode = null) => {
   // Store the parameters in private variables
-  let _storedValue = value;
-  let _storedNode = nextNode;
+  let storedValue = value;
+  let storedNode = nextNode;
 
   // Methods to get the stored info
-  const getValue = () => _storedValue;
-  const getNextNode = () => _storedNode;
+  const getValue = () => storedValue;
+  const getNextNode = () => storedNode;
 
   // Methods to set the stored info
   const setValue = (newValue) => {
-    _storedValue = newValue;
+    storedValue = newValue;
   };
   const setNextNode = (newNextNode) => {
-    _storedNode = newNextNode;
+    storedNode = newNextNode;
   };
 
   return {
@@ -20,4 +20,46 @@ const nodeFactory = (value = null, nextNode = null) => {
   };
 };
 
-const linkedListFactory = () => ({});
+const linkedListFactory = () => {
+  // Initialize head, tail, and length
+  let head = null;
+  let tail = null;
+  let listLength = 0;
+
+  const getLength = () => listLength;
+  const getHead = () => head;
+  const getTail = () => tail;
+
+  // Method to append new node
+  const append = (value) => {
+    // Create node with given value
+    const newNode = nodeFactory(value);
+    // If first node, will place it at the head
+    if (head === null) {
+      head = newNode;
+    } else {
+      tail = newNode;
+    }
+    // Increase length counter
+    listLength += 1;
+  };
+
+  return {
+    append, getLength, getHead, getTail,
+  };
+};
+
+const testNode = nodeFactory('ooh yeah');
+console.log(testNode.getValue());
+testNode.setValue('Ooompah');
+console.log(testNode.getValue());
+
+const testList = linkedListFactory();
+console.log(testList);
+console.log(testList.getLength());
+testList.append('hello');
+console.log(testList.getLength());
+console.log(testList.getHead().getValue());
+console.log(testList.getTail());
+testList.append('I so pale');
+console.log(testList.getTail().getValue());
